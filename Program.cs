d6d58cdeebfeec9f96f2b63fbfa8175122c0a8a5
Shah669 <122481622+Shah669@users.noreply.github.com>
2023-01-26 17:53:43 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace Deliverable__3____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int input = 0;
+                string series = "";
+
+                while (true)
+                {
+
+                    Console.WriteLine("Enter an integer number between 1 to 100: ");
+                    input = int.Parse(Console.ReadLine());
+
+                    if (input >= 1 && input <= 100)
+                    {
+                        break;
+                    }
+                }
+
+                while (true)
+                {
+                    Console.Write("Specify the series type: Even or Odd ");
+                    series = Console.ReadLine();
+
+                    if (series == "Even" || series == "Odd")
+                    {
+                        break;
+                    }
+                }
+
+                if (series == "Odd")
+                {
+
+                    Console.WriteLine("You have selected " + series + " series. The numbers between 0 and " + input + " are:");
+                    for (int i = 1; i <= input; i = i + 2)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+                if (series == "Even")
+                {
+
+                    Console.WriteLine("You have selected " + series + " series. The numbers between 0 and " + input + " are:");
+                    for (int i = 0; i <= input; i = i + 2)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Enter an integer value");
+            }
+        }
+    }
+    
+}
